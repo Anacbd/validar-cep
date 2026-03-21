711a09e4e373da1cf7d2b8bd4169ff8e01fac78c
ANA CAROLINA COSTA ABBUD <00126629@unisoacad.uniso.br>
2026-03-20 21:36:32 -0300

    actions

---
+++ test_cadastro.py
@@ -0,0 +1,30 @@
+import pytest
+from validador_cep import validar_cep
+
+
+@pytest.mark.parametrize("cep, cidade, esperado", [
+
+    #válidos
+    ("18110-000", "Votorantim", True),
+    ("18110000", "Votorantim", True),
+    ("18000-000", "Sorocaba", True),
+    ("18000000", "Sorocaba", True),
+
+    #case insensitive
+    ("18110-000", "votorantim", True),
+    ("18110-000", "VOTORANTIM", True),
+
+    #inválidos
+    ("18110-000", "Sorocaba", False),
+    ("00000-000", "Cidade", False),
+    ("abcde", "Sorocaba", False),
+
+    #entradas inválidas
+    ("", "Sorocaba", False),
+    ("18000000", "", False),
+    (None, "Sorocaba", False),
+
+])
+def test_validar_cep(cep, cidade, esperado):
+
+    assert validar_cep(cep, cidade) == esperado
